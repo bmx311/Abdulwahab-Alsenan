551bd153eac560d7a3918178837f89a69aa12cd8
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-11 19:24:48 +0300

    added both type interference and val vs var exercises (cuz it was getting repetitive)

---
+++ type_interference.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var city = "New York"
+    var temperature = "22.5"
+
+    println("the type of city is " + city.javaClass.kotlin)
+    println("the type of temperature is " + temperature.javaClass.kotlin)
+}
---
+++ val_vs_var.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var score = 10
+    score = 20
+    val greeting = "Hello, Kotlin!"
+    greeting = "hello, Wahab!" //<-- error: Val cannot be re-assigned
+}
