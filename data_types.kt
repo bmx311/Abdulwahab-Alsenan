870a3c919cbadd5ced5d44622fdac6f192e30197
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-11 19:22:50 +0300

    added data types exercise

---
+++ data_types.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var temperature:Float = 36.5F
+    var rainBool:Boolean = false
+    var initial:Char = 'W'
+
+    println("the temperature is $temperature.")
+    println("is it raining? $rainBool.")
+    println("the first letter of my (short) name is $initial.")
+}
